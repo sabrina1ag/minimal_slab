ef87bb54985aa395f7913c6a88ecb8d2b555c07f
Binoulll <121357274+sabrina1ag@users.noreply.github.com>
2026-01-07 19:17:56 +0100

    Create Slab empty struct

--- src/slab.rs
+++ src/slab.rs
@@ -1,2 +1,10 @@
 //! # Slab
 //! Chaque slab maintient une liste libre des objets disponibles.
+use core::ptr::NonNull;
+use crate::page_allocator::PAGE_SIZE;
+
+/// Un slab gère un bloc de mémoire divisé en objets de taille fixe.
+///
+/// Le slab utilise une liste libre chaînée pour suivre les objets disponibles.
+/// Chaque objet libre contient un pointeur vers le prochain objet libre.
+pub struct Slab;
