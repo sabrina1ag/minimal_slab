e4bcc8869ec0d98274c2ddf0df3a7999b026126a
sabriiiiina <s_fergui@estin.dz>
2026-01-11 09:44:47 +0100

    ajout du Test : création d'un slabCache

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,16 @@ extern crate alloc;
 pub mod page_allocator;
 pub mod slab;
 pub mod slab_cache;
+
+#[cfg(test)]
+mod tests {
+    use super::slab_cache::SlabCache;
+
+    #[test]
+    fn slab_cache_creation() {
+        let cache = SlabCache::new(32);
+        assert_eq!(cache.object_size(), 32);
+        assert_eq!(cache.slab_count(), 0);
+        assert_eq!(cache.total_allocated(), 0);
+    }
+}
